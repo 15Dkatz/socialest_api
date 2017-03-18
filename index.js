de064057c128d48d3031165f7b84ea9478ed623c
davidkatz <15Dkatz@shcp.edu>
2017-03-18 14:48:52 -0700

    add api

---
+++ index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+var fs = require('fs');
+
+var config = {
+    apiKey: "AIzaSyCBXjXTPiYdH5YmwjqGkYEBc3P3oRSEs8U",
+    authDomain: "socialest-f3e42.firebaseapp.com",
+    databaseURL: "https://socialest-f3e42.firebaseio.com",
+    storageBucket: "socialest-f3e42.appspot.com",
+    messagingSenderId: "171671691972"
+};
+
+var firebaseApp = firebase.initializeApp(config);
+var images_ref = firebase.database().ref().child('images');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.json({home: 'home'})
+})
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  var response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
+
+// one post request, send image,
+// then sends back the aggregated image
+// app.post...
+app.post('/post_image', function(req, res) {
+  console.log('aggregate the image!');
+
+  // console.log('req.body', req.body);
+
+  let { image } = req.body;
+
+  var imageBuffer = decodeBase64Image(image);
+
+
+  // upload the iamge
+  images_ref.push({image});
+
+  images_ref.once('value', snap => {
+    let images = [];
+    snap.forEach(i => {
+      const f_image = i.val().image;
+      images.push(f_image);
+    })
+    console.log('images.length', images.length);
+    var image_string = './images/image_' + images.length + '.png';
+    // console.log('imageBuffer.data', imageBuffer.data);
+    console.log('image_string', image_string);
+
+    fs.writeFile(image_string, imageBuffer.data, function(err) {
+      console.log('well actually check the folder');
+    })
+  })
+  // write the file to the images folder on the local system, which will be hosted on heroku
+  res.json({success: 'success'})
+
+})
+
+
+
+app.listen(process.env.PORT || '4000', function() {
+  console.log(`Express server listening on port ${this.address().port} in ${app.settings.env} mode.`)
+})
